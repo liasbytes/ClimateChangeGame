e948d601247e2e2d6137b3bc5acc02fc12137cc2
Lia <zshep31415@outlook.com>
2024-03-26 10:57:24 -0700

    Save/Load Controller

---
+++ Climate Change Game/Assets/Scripts/DataManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+// Some file writing code adapted from https://github.com/UnityTechnologies/UniteNow20-Persistent-Data
+public class DataManager : MonoBehaviour
+{
+    public PlayerController playerData;
+    public InventorySystem inventoryData;
+
+    public void writeSaveData()
+    {
+        SaveData data = new SaveData();
+        data.health = playerData.health;
+        data.cpNum = playerData.cpNum;
+        data.checkpointLocation = playerData.respawnLocation;
+        data.inventory = inventoryData.inventory;
+        string JSONData = JsonUtility.ToJson(data);
+        var fullPath = Path.Combine(Application.persistentDataPath, "ClimateChangeSaveData.json");
+        try
+        {
+            File.WriteAllText(JSONData, fullPath);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to read from {fullPath} with exception {e}");
+        }
+        
+    }
+
+    public void readSaveData()
+    {
+        var fullPath = Path.Combine(Application.persistentDataPath, "ClimateChangeSaveData.json");
+        string JSONData = "";
+        try
+        {
+            JSONData = File.ReadAllText(fullPath);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to read from {fullPath} with exception {e}");
+        }
+        SaveData data = JsonUtility.FromJson<SaveData>(JSONData);
+        playerData.health = data.health;
+        playerData.cpNum = data.cpNum;
+        playerData.respawnLocation = data.checkpointLocation;
+        playerData.transform.position = data.checkpointLocation.position;
+        inventoryData.inventory = data.inventory;
+
+    }
+
+}
